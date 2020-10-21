4cccb787b5d2f54371cf4d17dcc5ba6e0255eae9
TAJAN Romain <Romain.Tajan@bordeaux-inp.fr>
2020-10-21 11:39:41 +0200

    Remove de2bi from the example.

--- example.m
+++ example.m
@@ -2,7 +2,9 @@ clear
 clc
 close all
 
-H = de2bi(1:7,3)'; % The parity check matrix for Hamming code
+H = [1 0 1 0 1 0 1;0 1 1 0 0 1 1;0 0 0 1 1 1 1]; % The parity check matrix for Hamming code
+
+
 tg = tanner_graph(H); % Building the Tanner graph
 plot(tg) % Display the Tanner graph
 
